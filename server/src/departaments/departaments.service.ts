8133bdf8e830a28e4a7eaf0b2e3851cea37c163a
karabass92 <shatveryank@gmail.com>
2023-12-18 20:46:03 +0600

    Добавлена валидация входных данных для методов positionService и departamentService

--- server/src/departaments/departaments.service.ts
+++ server/src/departaments/departaments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, BadRequestException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Departament } from './departaments.entity'
@@ -12,9 +12,36 @@ export class DepartamentService {
         @InjectRepository(Departament) private readonly departamentRepository: Repository<Departament>
     ) { }
 
+    private async validateDepartament(name?: string, id?: number): Promise<void> {
+        try {
+            if (name) {
+                const modifiedName = this.modifyName(name)
+                const departamentName = await this.departamentRepository.findOneBy({ name: modifiedName })
+                if (departamentName) throw new BadRequestException('Подразделение с таким наименованием уже заведено!')
+            }
+            if (id) {
+                const departamentId = this.departamentRepository.findOneBy({ id: id })
+                if (!departamentId) throw new BadRequestException('Подразделение с таким id номером не найдено!')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    private modifyName(name: string): string {
+        try {
+            const modifiedName = name.trim().toLowerCase()
+            return modifiedName.charAt(0).toUpperCase() + modifiedName.slice(1)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     async create(createDepartamentInput: CreateDepartamentInput): Promise<Departament> {
         try {
-            return await this.departamentRepository.save(createDepartamentInput)
+            const name = this.modifyName(createDepartamentInput.name)
+            await this.validateDepartament(name)
+            return await this.departamentRepository.save({ name })
         } catch (error) {
             console.log(error)
         }
@@ -22,20 +49,22 @@ export class DepartamentService {
 
     async update(updateDepartamentInput: UpdateDepartamentInput): Promise<Departament> {
         try {
+            const name = this.modifyName(updateDepartamentInput.name)
+            await this.validateDepartament(name, updateDepartamentInput.id)
+            await this.departamentRepository.update(
+                { id: updateDepartamentInput.id },
+                { name: name }
+            )
             return await this.getOne(updateDepartamentInput.id)
         } catch (error) {
             console.log(error)
         }
-        await this.departamentRepository.update(
-            { id: updateDepartamentInput.id },
-            {
-                name: updateDepartamentInput.name
-            }
-        )
+
     }
 
     async remove(id: number): Promise<number> {
         try {
+            await this.validateDepartament('', id)
             await this.departamentRepository.delete({ id })
             return id
         } catch (error) {
@@ -45,6 +74,7 @@ export class DepartamentService {
 
     async getOne(id: number): Promise<Departament> {
         try {
+            await this.validateDepartament('', id)
             return await this.departamentRepository.findOne({ where: { id } })
         } catch (error) {
             console.log(error)
--- server/src/positions/positions.service.ts
+++ server/src/positions/positions.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, BadRequestException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Position } from './positions.entity'
@@ -12,8 +12,35 @@ export class PositionService {
         @InjectRepository(Position) private readonly positionRepository: Repository<Position>
     ) { }
 
+    private async validatePosition(name?: string, id?: number): Promise<void> {
+        try {
+            if (name) {
+                const modifiedName = this.modifyName(name)
+                const positionName = await this.positionRepository.findOneBy({ name: modifiedName })
+                if (positionName) throw new BadRequestException('Должность с таким наименованием уже заведена!')
+            }
+            if (id) {
+                const positionId = this.positionRepository.findOneBy({ id: id })
+                if (!positionId) throw new BadRequestException('Должность с таким id номером не найдена!')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    private modifyName(name: string): string {
+        try {
+            const modifiedName = name.trim().toLowerCase()
+            return modifiedName.charAt(0).toUpperCase() + modifiedName.slice(1)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     async create(createPositionInput: CreatePositionInput): Promise<Position> {
         try {
+            const name = this.modifyName(createPositionInput.name)
+            await this.validatePosition(name)
             return await this.positionRepository.save(createPositionInput)
         } catch (error) {
             console.log(error)
@@ -22,20 +49,22 @@ export class PositionService {
 
     async update(updatePositionInput: UpdatePositionInput): Promise<Position> {
         try {
+            const name = this.modifyName(updatePositionInput.name)
+            await this.validatePosition(name, updatePositionInput.id)
+            await this.positionRepository.update(
+                { id: updatePositionInput.id },
+                { name: name }
+            )
             return await this.getOne(updatePositionInput.id)
         } catch (error) {
             console.log(error)
         }
-        await this.positionRepository.update(
-            { id: updatePositionInput.id },
-            {
-                name: updatePositionInput.name
-            }
-        )
+
     }
 
     async remove(id: number): Promise<number> {
         try {
+            await this.validatePosition('', id)
             await this.positionRepository.delete({ id })
             return id
         } catch (error) {
@@ -45,6 +74,7 @@ export class PositionService {
 
     async getOne(id: number): Promise<Position> {
         try {
+            await this.validatePosition('', id)
             return await this.positionRepository.findOne({ where: { id } })
         } catch (error) {
             console.log(error)
